eaa4c2865e25c64f3cb6cd49944b2a64a74986cf
Mateus <mtex613@gmail.com>
2022-06-21 14:39:10 -0300

    Atualizando o angular

--- world-map-covid/src/app/app.module.ts
+++ world-map-covid/src/app/app.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
-
+import { NgxEchartsModule } from 'ngx-echarts';
 @NgModule({
   declarations: [
     AppComponent,
-    MapComponent
+    MapComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxEchartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
